7a94d8bf8f61e10f5ed19485743d10bf8d0ebb91
aershov <codumentary.com@gmail.com>
2017-01-01 22:46:51 +0300

    Add basic template

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+extern crate bmp;
+
+use bmp::{Image, Pixel};
+
 fn main() {
-    println!("Hello, world!");
+    let mut image = Image::new(256, 256);
+    draw_line(&mut image, 0, 200);
+    image.save("/Users/aershov182/tmp/raytracer.bmp").expect("oops");
+}
+
+fn draw_line(image: &mut Image, start: u32, end: u32) {
+    for i in start..end {
+        image.set_pixel(i, i, Pixel { r: 255, g: 255, b: 255 });
+    }
 }
