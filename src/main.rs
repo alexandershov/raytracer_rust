de95f6093abac5afe6a377d7dd8046110b419924
aershov <codumentary.com@gmail.com>
2017-01-02 14:50:35 +0300

    add main driver

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,46 @@
 extern crate bmp;
 extern crate raytracer;
 
-
 use bmp::{Image, Pixel};
-use raytracer::{Floor, Color, Point};
+use raytracer::{Floor, Color, Point, Plane, Ray};
+
+struct Scene {
+    floor: Floor,
+    eye: Point,
+}
 
 fn main() {
-    let mut image = Image::new(256, 256);
-    draw_floor(&mut image);
+    let size = 200;
+    let mut image = Image::new(size, size);
+    let floor = Floor::new(16.0);
+    let floor_plane = Plane { a: 0.0, b: 0.0, c: 1.0, d: 0.0 };
+    let scene = Scene {
+        floor: floor,
+        eye: Point { x: size / 2, y: size / 2, z: size / 2 },
+    };
+    for x in 0..size {
+        for z in 0..size {
+            let ray = Ray {
+                start: eye,
+                direction: Point {
+                    x: x - eye.x,
+                    y: 0 - eye.y,
+                    z: z - eye.z,
+                },
+            };
+            let points = floor_plane.get_intersections(ray);
+            let color;
+            if points.len() == 0 {
+                color = Color { r: 0, g: 0, b: 180 }
+            } else {
+                color = floor.color_at(points[0]);
+            }
+            image.set_pixel(x, z, color_to_pixel(color));
+        }
+    }
     image.save("/Users/aershov182/tmp/raytracer.bmp").expect("oops");
 }
 
-fn draw_floor(image: &mut Image) {
-    let floor = Floor::new(16.0);
-    for x in 0..255 {
-        for y in 0..255 {
-            let point = Point { x: x as f32, y: y as f32, z: 0.0 };
-            let color = floor.color_at(point);
-            image.set_pixel(x, y, color_to_pixel(color));
-        }
-    }
-}
 
 fn color_to_pixel(color: Color) -> Pixel {
     return Pixel {
