46126b809b78bdd85feec8cce2de62d107e1cb1c
aershov <codumentary.com@gmail.com>
2017-01-02 13:28:31 +0300

    save floor in bmp

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,31 @@
 extern crate bmp;
+extern crate raytracer;
+
 
 use bmp::{Image, Pixel};
+use raytracer::{Floor, Color, Point};
 
 fn main() {
     let mut image = Image::new(256, 256);
-    draw_line(&mut image, 0, 200);
+    draw_floor(&mut image);
     image.save("/Users/aershov182/tmp/raytracer.bmp").expect("oops");
 }
 
-fn draw_line(image: &mut Image, start: u32, end: u32) {
-    for i in start..end {
-        image.set_pixel(i, i, Pixel { r: 255, g: 255, b: 255 });
+fn draw_floor(image: &mut Image) {
+    let floor = Floor::new(16.0);
+    for x in 0..255 {
+        for y in 0..255 {
+            let point = Point { x: x as f32, y: y as f32, z: 0.0 };
+            let color = floor.color_at(point);
+            image.set_pixel(x, y, color_to_pixel(color));
+        }
+    }
+}
+
+fn color_to_pixel(color: Color) -> Pixel {
+    return Pixel {
+        r: color.r,
+        g: color.g,
+        b: color.b,
     }
 }
